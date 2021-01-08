1771c51c4d8b77f6e84b0e026dd7d64565e77f01
roody95 <agbassahr@gmail.com>
2021-01-08 11:27:20 +0000

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,18 +8,22 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   slidesOptions = {
-    initialSlide: 0,
+    // initialSlide: 0,
     direction: 'horizontal',
     speed: 300,
     // effect: slide,
     spaceBetween: 8,
-    slidesPerView: 1.5,
+    slidesPerView: 1.2,
     freeMode: true,
     loop: true
   };
   
   slides = [
-    { img: '../../assets/img/1.jpg'}
+    { img: '../../assets/img/1.jpg'},
+    { img: '../../assets/img/2.jpg'},
+    { img: '../../assets/img/3.jpg'},
+    { img: '../../assets/img/4.jpg'},
+    { img: '../../assets/img/5.jpg'},
   ];
 
   
