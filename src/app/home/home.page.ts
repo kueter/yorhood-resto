928d0a104bb9d10e984e0ff0333fa634da15b4c0
roody95 <agbassahr@gmail.com>
2021-01-08 11:15:51 +0000

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,8 +7,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  slideOptions = {};
+  slidesOptions = {
+    initialSlide: 0,
+    direction: 'horizontal',
+    speed: 300,
+    // effect: slide,
+    spaceBetween: 8,
+    slidesPerView: 1.5,
+    freeMode: true,
+    loop: true
+  };
+  
+  slides = [
+    { img: '../../assets/img/1.jpg'}
+  ];
 
+  
   constructor() {}
 
   onSearchChange(event) {
