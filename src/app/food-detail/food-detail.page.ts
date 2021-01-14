b4347df215aa7a42e14a800589d332d1857f013e
roody95 <agbassahr@gmail.com>
2021-01-14 19:28:03 +0000

    commit

--- src/app/food-detail/food-detail.page.ts
+++ src/app/food-detail/food-detail.page.ts
@@ -13,12 +13,14 @@ export class FoodDetailPage implements OnInit {
   food: any;
   reduce = false;
   qte = 1;
+  mount: any;
 
   constructor(private route: ActivatedRoute, public service: DataService) {
     
     this.route.params.subscribe(
       (_: any) => {
          this.food = this.service.foods.filter((data: any) => data?.id == _?.id);
+         this.mount = this.food[0]?.price
       }
     );
   }
@@ -27,5 +29,13 @@ export class FoodDetailPage implements OnInit {
     
   }
 
+  increase() {
+    this.qte ++;
+
+  }
+
+  decrease() {
+    this.qte >= 2 ? this.qte-- : this.qte = 1;
+  }
  
 }
