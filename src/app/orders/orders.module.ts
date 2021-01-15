23bef23fe26ac26aab431c27e15bd355a3ec1379
roody95 <agbassahr@gmail.com>
2021-01-15 08:28:51 +0000

    commit

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { OrdersPageRoutingModule } from './orders-routing.module';
 
 import { OrdersPage } from './orders.page';
+import { ExpandableComponent } from '../components/expandable/expandable.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { OrdersPage } from './orders.page';
     IonicModule,
     OrdersPageRoutingModule
   ],
-  declarations: [OrdersPage]
+  declarations: [OrdersPage,ExpandableComponent]
 })
 export class OrdersPageModule {}
