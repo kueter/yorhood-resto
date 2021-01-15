3c7230f4d881a5c09ed8d7178164dd084f40edcf
roody95 <agbassahr@gmail.com>
2021-01-15 10:34:56 +0000

    commit

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { map } from 'rxjs/operators' ;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  private itemsInCartSubject: BehaviorSubject<any[]> = new BehaviorSubject([]);
+  private itemsInCart: any[] = [];
+
+  constructor() {
+    this.itemsInCartSubject.subscribe(_ => this.itemsInCart = _);
+  }
+
+  public addToCart(item: any) {
+    this.itemsInCartSubject.next([...this.itemsInCart, item]);
+  }
+
+  public removeFromCart(item: any) {
+    const currentItems = [...this.itemsInCart];
+    const itemsWithoutRemoved = currentItems.filter(_ => _.name !== item.name);
+    this.itemsInCartSubject.next(itemsWithoutRemoved);
+  }
+
+  public getItems(): Observable<any[]> {
+    return this.itemsInCartSubject.asObservable();
+  }
+
+  public getTotalAmount(): Observable<number> {
+    return this.itemsInCartSubject.pipe(map((items: any[]) => {
+      return items.reduce((prev, curr: any) => {
+        return prev + curr?.cost;
+      }, 0);
+    })
+  );
+  }
+
+  public clearCart() {
+    this.itemsInCartSubject.next([]);
+  }
 }
