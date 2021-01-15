8f19042eab93f092f13ca866b182b0bb994f1fd4
roody95 <agbassahr@gmail.com>
2021-01-15 19:19:59 +0000

    commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmptyComponent } from '../components/empty/empty.component';
+
+
+@NgModule({
+  declarations: [ EmptyComponent ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
