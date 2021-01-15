8df0473227074f1448f495447fa37b1e027936b7
roody95 <agbassahr@gmail.com>
2021-01-15 10:24:39 +0000

    commit

--- src/app/shopping-cart/shopping-cart.page.ts
+++ src/app/shopping-cart/shopping-cart.page.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.page.scss'],
 })
 export class ShoppingCartPage implements OnInit {
-
+  
+  subtotal:any;
+  byTax:any;
   total_order: any;
   
   constructor() { }
