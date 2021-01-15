cd86ece7344807938be270b2f67322dbfd49ce93
roody95 <agbassahr@gmail.com>
2021-01-15 09:50:52 +0000

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
 import { IonTabs } from '@ionic/angular';
 
 @Component({
@@ -7,12 +7,20 @@ import { IonTabs } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit  {
   @ViewChild('tabs', { static: false }) tabs: IonTabs;
   selectedTab: any;
 
+
   
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+
+  }
+
+
+  ngOnInit() {
+    
+  }
 
   onSearchChange(event: any) {
     console.log(event.target.value);
