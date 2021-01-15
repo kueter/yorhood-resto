7da68bb4f4bdbc8a7b3fc3001d825d0288bd54be
roody95 <agbassahr@gmail.com>
2021-01-15 19:29:05 +0000

    commit

--- src/app/shopping-cart/shopping-cart.module.ts
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ShoppingCartPageRoutingModule } from './shopping-cart-routing.module';
 
 import { ShoppingCartPage } from './shopping-cart.page';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ShoppingCartPageRoutingModule
+    ShoppingCartPageRoutingModule,
+    SharedModule
   ],
   declarations: [ShoppingCartPage]
 })
